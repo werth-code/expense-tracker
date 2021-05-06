c4abb2bfdbabc0da8e544cd043ff30829018899c
Matt Werth <matthewwerth@gmail.com>
2021-05-06 08:48:07 -0400

    Added Expense Class

--- src/main/java/com/matthewwerth/expensetracker/model/Category.java
+++ src/main/java/com/matthewwerth/expensetracker/model/Category.java
@@ -2,9 +2,8 @@ package com.matthewwerth.expensetracker.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 
 @Entity
 @Data //Lombok auto create getters/setters etc.
@@ -17,5 +16,9 @@ public class Category {
 
     // categories like travel, phone bill, internet
     private String name;
+
+    @ManyToOne(cascade = CascadeType.PERSIST) //
+    // many categories connected to one user
+    private User user;
 
 }
---
+++ src/main/java/com/matthewwerth/expensetracker/model/Expense.java
@@ -0,0 +1,37 @@
+package com.matthewwerth.expensetracker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="expense")
+
+public class Expense {
+    @Id
+    private Long id;
+    private Instant expenseDate;
+    private String description;
+    //private Double amount;
+
+    // now we need to connect to both user and have a category
+
+    @ManyToOne // this is like a sql join but JPA handles it for us
+    private Category category;
+
+    @ManyToOne // many expenses go to one user.
+    private User user;
+
+
+
+
+}
