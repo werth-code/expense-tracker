90eb5d63616d797b4b4484fd1af6e12e05b0dd47
Matt Werth <matthewwerth@gmail.com>
2021-05-06 15:58:51 -0400

    Fixed Getter-Setter issue

---
+++ expense-tracker/src/Category.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class Category extends Component {
+
+    state = {
+        isLoading : true,
+        Categories : []
+    }
+    // sync - we wait for a response.
+    // async - we don't wait, but when we get a response we do something.
+
+    async componentDidMount() {
+        const response = await fetch('/api/categories')
+        const body = await response.json()
+        this.setState({ Categories : body, isLoading : false }) //set the state to the response and isLoading to false
+    }
+
+    render() {
+
+        const { Categories, isLoading } = this.state; 
+        if(isLoading) return (<div>Loading...</div>)
+
+        return (
+            <div>
+                <h1>Categories</h1>
+                {
+                    Categories.map( category => 
+                    <div key={category.id} id={ category.id }>
+                        { category.name }
+                    </div>)
+                }
+            </div>
+        )
+    }
+}
+
--- src/main/java/com/matthewwerth/expensetracker/model/Category.java
+++ src/main/java/com/matthewwerth/expensetracker/model/Category.java
@@ -7,7 +7,6 @@ import lombok.NonNull;
 import javax.persistence.*;
 
 @Entity
-@Data //Lombok auto create getters/setters etc.
 @NoArgsConstructor
 @Table(name="category")
 public class Category {
@@ -19,4 +18,19 @@ public class Category {
     @NonNull
     private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
