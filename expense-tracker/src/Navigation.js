bd06d34bdd614a1a5d86183500869370961465f3
Matt Werth <matthewwerth@gmail.com>
2021-05-10 09:53:11 -0400

    Navigation Tweaks

--- expense-tracker/src/Navigation.js
+++ expense-tracker/src/Navigation.js
@@ -1,29 +1,57 @@
-import React, { Component } from 'react';
-import {Nav, Navbar, NavItem, NavbarBrand, NavLink} from 'reactstrap';
+import React, { useState } from 'react';
+import {Nav, Navbar, NavItem, NavbarBrand, NavLink, NavbarToggler, Collapse} from 'reactstrap';
+import "./Navigation"
 
 
-class Navigation extends Component {
-    state = {  }
-    render() {
-        return (
-          <div>
-            <Navbar color="dark" dark  expand="md">
-              <NavbarBrand href="/">Expense Tracker App</NavbarBrand>
-                <Nav className="ml-auto" navbar>
-                  <NavItem>
-                    <NavLink href="/">Home</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="/categories">Categories</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="/expenses">Expenses</NavLink>
-                  </NavItem>
-                </Nav>
-            </Navbar>
-          </div>
-        );
-      }
+const Navigation = () => {
+
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => (setCollapsed(!collapsed))
+   
+
+    return (
+      <div id='main-nav'>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">Expense Tracker</NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/expenses">Expenses</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+
+      
 }
  
 export default Navigation;
+
+
+        // return (
+        //   <div>
+        //     <Navbar id="main-nav" color="dark" dark  expand="md">
+        //       <NavbarBrand className="mr-auto" href="/">Expense Tracker App</NavbarBrand>
+        //         {/* <NavbarToggler onClick={toggleNavbar} className="mr-2" /> */}
+        //         <Collapse isOpen={!collapsed} navbar></Collapse>
+        //         <Nav navbar>
+        //           <NavItem>
+        //             <NavLink href="/">Home</NavLink>
+        //           </NavItem>
+        //           <NavItem>
+        //             <NavLink href="/categories">Categories</NavLink>
+        //           </NavItem>
+        //           <NavItem>
+        //             <NavLink href="/expenses">Expenses</NavLink>
+        //           </NavItem>
+        //         </Nav>
+        //     </Navbar>
+        //   </div>
+        // );
